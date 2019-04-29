587b590fa3088406333e9cf25cfcad198f9d776c
Abdullah Albannay <aabannay@gmail.com>
2019-04-29 21:22:39 +0300

    updated SolnSquareVerifier contract

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -7,7 +7,7 @@ import './ERC721Mintable.sol';
 
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
-contract SolnSquareVerifier is CustomERC721Token {
+contract SolnSquareVerifier is CustomERC721Token, Verifier {
     // TODO define a solutions struct that can hold an index & an address
     struct Solution {
         uint256 tokenId; 
@@ -24,10 +24,9 @@ contract SolnSquareVerifier is CustomERC721Token {
     event SolutionAdded(address owner, uint256 tokenId);
     
     // TODO Create a function to add the solutions to the array and emit the event
-    function addUniqueSolution(address owner, uint256 tokenId) public {
+    function addUniqueSolution(address owner, uint256 tokenId, bytes32 unique) public {
         //TODO fill the function
-        bytes32 unique = keccak256(abi.encodePacked(owner, tokenId));
-        require(uniqueSolutions[unique].owner != address(0), 'Solution added already!'); 
+        require(uniqueSolutions[unique].owner == address(0), 'Solution exists already!');
         uniqueSolutions[unique] = Solution({tokenId: tokenId, owner: owner}); 
         emit SolutionAdded(owner, tokenId);
     }
@@ -36,12 +35,26 @@ contract SolnSquareVerifier is CustomERC721Token {
     // TODO Create a function to mint new NFT only after the solution has been verified
     //  - make sure the solution is unique (has not been used before)
     //  - make sure you handle metadata as well as tokenSuplly
-    function mintNFT(address owner, uint256 tokenId) public onlyOwner() {
-        bytes32 unique = keccak256(abi.encodePacked(owner, tokenId));
-        require(uniqueSolutions[unique].owner != address(0), 'solution is not unique');
-        addUniqueSolution(owner, tokenId);
+    function mintNFT(address owner, uint256 tokenId, string memory tokenURI,
+            /* added from verifier class (all verification parameters) */
+            uint[2] memory a,
+            uint[2] memory a_p,
+            uint[2][2] memory b,
+            uint[2] memory b_p,
+            uint[2] memory c,
+            uint[2] memory c_p,
+            uint[2] memory h,
+            uint[2] memory k,
+            uint[2] memory input) public onlyOwner() {
+        
+        bool valid = super.verifyTx(a, a_p, b, b_p, c, c_p, h, k, input);
+        require(valid, 'Solution is not valid!');
+        
+        bytes32 unique = keccak256(abi.encodePacked(a, a_p, b, b_p, c, c_p, h, k, input));
+        addUniqueSolution(owner, tokenId, unique);
 
         //TODO handle metadata and tokenSupply!
+        //this is handled by inhiretance. 
     }
 
 }
